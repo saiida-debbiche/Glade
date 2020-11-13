8dfbdf38c9de71ae32b3f2bc073caf06b685875e
saiida-debbiche <saiida.debbiche@esprit.tn>
2020-11-13 11:59:43 +0100

    ajout

---
+++ callbacks.c
@@ -0,0 +1,66 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_buttonSD2_clicked                   (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_buttonSD4_clicked                   (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_buttonSD3_clicked                   (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_buttonSD5_clicked                   (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_buttonSD6_clicked                   (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_buttonSD7_clicked                   (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_buttonSD9_clicked                   (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
---
+++ callbacks.h
@@ -0,0 +1,30 @@
+#include <gtk/gtk.h>
+
+
+void
+on_buttonSD2_clicked                   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonSD4_clicked                   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonSD3_clicked                   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonSD5_clicked                   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonSD6_clicked                   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonSD7_clicked                   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonSD9_clicked                   (GtkButton       *button,
+                                        gpointer         user_data);
